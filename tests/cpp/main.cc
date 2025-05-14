0b2c8ff5b208a199ff052cdc3b1aa2a53d6d3fbb
ConvolutedDog <yangjianchao16@nudt.edu.cn>
2025-05-14 12:21:43 +0800

    [Test] Add ir/transform test

--- tests/cpp/main.cc
+++ tests/cpp/main.cc
@@ -30,7 +30,7 @@ int main() {
   // std::cout << "NDArrayTest passed!\n\n";
 
   // std::cout << "Running AutoSchedulerTest...\n";
-  // // InplaceArrayBaseTest(); // Bug in MacOS
+  // // InplaceArrayBaseTest(); // May have Bug in MacOS
   // std::cout << "AutoSchedulerTest passed!\n\n";
 
   // std::cout << "Running ObjectTest...\n";
