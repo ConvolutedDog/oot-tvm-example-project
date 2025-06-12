81f0e9bb6d7ad69304bf250c4fa3a4a0c5a7a57b
BenkangPeng <benkangpeng@163.com>
2025-06-12 10:38:49 +0800

    [TEST] Add the test of analysis.

---
+++ tests/cpp/include/relax/analysis-test.h
@@ -0,0 +1,35 @@
+#include "utils.h"
+#include <tvm/arith/analyzer.h>
+#include <tvm/ir/expr.h>
+#include <tvm/relax/analysis.h>
+#include <tvm/relax/expr.h>
+#include <tvm/relax/struct_info.h>
+#include <tvm/relax/type.h>
+#include <tvm/runtime/container/map.h>
+#include <tvm/runtime/data_type.h>
+#include <tvm/runtime/registry.h>
+
+namespace analysis_test {
+using tvm::DataType;
+using tvm::PrimExpr;
+using tvm::arith::Analyzer;
+using tvm::relax::BaseCheckResult;
+using tvm::relax::CanProveShapeEqual;
+using tvm::relax::GetStaticType;
+using tvm::relax::ObjectStructInfo;
+using tvm::relax::PrimStructInfo;
+using tvm::relax::ShapeExpr;
+using tvm::relax::ShapeStructInfo;
+using tvm::relax::StructInfo;
+using tvm::relax::StructInfoBaseCheck;
+using tvm::relax::StructInfoBaseCheckPrecondition;
+using tvm::relax::StructInfoFromType;
+using tvm::relax::TensorStructInfo;
+using tvm::relax::TensorType;
+using tvm::runtime::Map;
+using tvm::runtime::Registry;
+using tvm::With;
+void AnalysisTest();
+void BaseCheckTest();
+
+}  // namespace analysis_test
