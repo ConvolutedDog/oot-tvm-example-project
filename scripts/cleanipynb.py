bc7f4f5672f88b6eb3194e564e789d8bc92221b4
ConvolutedDog <yangjianchao16@nudt.edu.cn>
2025-06-18 21:39:06 +0800

    Update ipynb

--- scripts/cleanipynb.py
+++ scripts/cleanipynb.py
@@ -1,11 +1,60 @@
-# cleanipynb.py
+# A simple script to clean up Jupyter notebooks in place.
 import json
 import glob
-import sys
 import os
 
 
-def clean_notebook(file_path):
+def clean_notebook(file_path: str) -> None:
+    """
+    Clean up a Jupyter notebook file in place.
+
+    Args:
+        file_path (str): The path to the Jupyter notebook file.
+
+    Normal format of a Jupyter notebook is as follows:
+
+    {
+      "cells": [
+        {
+          "cell_type": "code",
+          "execution_count": null,
+          "id": "0",
+          "metadata": {},
+          "source": [
+            "import tvm\n",
+          ]
+        },
+        ...
+      ],
+      "metadata": {
+        "kernelspec": {
+          "display_name": "tvm-build-venv",
+          "language": "python",
+          "name": "python3"
+        },
+        "language_info": {
+          "codemirror_mode": {
+            "name": "ipython",
+            "version": 3
+          },
+          "file_extension": ".py",
+          "mimetype": "text/x-python",
+          "name": "python",
+          "nbconvert_exporter": "python",
+          "pygments_lexer": "ipython3",
+          "version": "3.11.13"
+        }
+      },
+      "nbformat": 4,
+      "nbformat_minor": 5
+    }
+
+    execution_count, id, metadata, outputs, metadata, nbformat, and nbformat_minor
+    are always modified to ensure the notebook is valid to run and it will cause the
+    git repo to be modified.
+
+    This script will modify the notebook file in place to remove these modifications.
+    """
     with open(file_path, "r", encoding="utf-8") as f:
         notebook = json.load(f)
 
@@ -18,17 +67,25 @@ def clean_notebook(file_path):
             cellid += 1
         if "metadata" in cell:
             cell["metadata"] = {}
-        if "outputs" in cell:
-            del cell["outputs"]
+        # if "outputs" in cell:
+        #     del cell["outputs"]
 
+    # if "metadata" in notebook:
+    #     del notebook["metadata"]
+
+    # if "nbformat" in notebook:
+    #     del notebook["nbformat"]
+
+    # if "nbformat_minor" in notebook:
+    #     del notebook["nbformat_minor"]
+    
     if "metadata" in notebook:
-        del notebook["metadata"]
-
-    if "nbformat" in notebook:
-        del notebook["nbformat"]
-
-    if "nbformat_minor" in notebook:
-        del notebook["nbformat_minor"]
+      if "kernelspec" in notebook["metadata"]:
+        if "display_name" in notebook["metadata"]["kernelspec"]:
+          notebook["metadata"]["kernelspec"]["display_name"] = ""
+      if "language_info" in notebook["metadata"]:
+        if "version" in notebook["metadata"]["language_info"]:
+          notebook["metadata"]["language_info"]["version"] = ""
 
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(notebook, f, indent=2, ensure_ascii=False)
