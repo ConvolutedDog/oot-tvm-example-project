95567c918432ac68b62e58c73d537341e5f4f342
ConvolutedDog <yangjianchao16@nudt.edu.cn>
2025-06-24 01:28:48 +0800

    [Relax-Test] Move some py to ipynb and fix several bugs of exec builder
    Co-authored-by: BenkangPeng <benkangpeng@163.com>

--- tests/python/tvm/relax/exec_builder.py
+++
@@ -1,8 +0,0 @@
-from tvm.relax import ExecBuilder
-
-eb = ExecBuilder()
-with eb.function("func0", num_inputs=2):
-    eb.emit_call("test.vm.add", args=[eb.r(0), eb.r(1)], dst=eb.r(2))
-    eb.emit_ret(eb.r(2))
-
-ex = eb.get()
